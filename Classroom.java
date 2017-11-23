65bf68002776efa3443e934569538b5b582f81ed
jdtheyoda <31745495+jdtheyoda@users.noreply.github.com>
2017-11-23 15:21:09 -0600

    Initial commit

---
+++ Classroom.java
@@ -0,0 +1,38 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.awt.*;
+/**
+ * This is my class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Classroom extends World
+{
+    
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public Classroom()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+        prepare();
+    }
+    
+    private void prepare()
+    {
+        Student student1 = new Student(1, "Nicholas", "Tanga", 11, 25.4, true);
+        
+        Student student2 = new Student(2, "Tanga", "Nathan", 11, 26.2, false);
+        
+        Student student3 = new Student(3, "Bob", "Builder", 11, 94.5, false); 
+        
+        addObject(student1, 60, 60);
+        
+        addObject(student2, 540, 60);
+        
+        addObject(student3, 300, 340);
+    }
+}
---
+++ Student.java
@@ -0,0 +1,175 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.awt.*;
+/**
+ * Write a description of class Student here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Student extends Actor
+{
+    int studentNumber;
+    String firstName;
+    String lastName;
+    int gradeLevel;
+    double average;
+    boolean it;
+    String upKey;
+    String downKey;
+    String leftKey;
+    String rightKey;
+    int itDelay;
+    
+    public Student(int number, String first, String last, int grade, double avg, boolean isIt)
+    {
+        studentNumber = number;                       
+        firstName = first;
+        lastName = last;
+        gradeLevel = grade;
+        average = avg;
+        it = isIt;
+        if(studentNumber == 1)
+        {
+            upKey = "w";
+            
+            downKey = "s";
+            
+            leftKey = "a";
+            
+            rightKey = "d";
+        }
+        else if(studentNumber == 2)
+        {
+            upKey = "u";
+            
+            downKey = "j";
+            
+            leftKey = "h";
+            
+            rightKey = "k";         
+        }
+        else
+        {
+            upKey = "up";
+            
+            downKey = "down";
+            
+            leftKey = "left";
+            
+            rightKey = "right";                                                
+        }
+        itDelay = 200;
+        if(it == true)
+        {
+            setImage("ppl1.png");
+        }
+    }
+    
+    public String getFirstName()
+    {
+        return firstName;
+    }
+    
+    public String getLastName()
+    {
+        return lastName;
+    }
+        
+    public int getGradeLevel()
+    {
+        return gradeLevel;
+    }
+    
+    public double getAverage()
+    {
+        return average;
+    }
+    
+    public boolean getit()
+    {
+        return it;
+    }
+    
+    public String toString()
+    {
+        return String.format("Name: %15s, %10s\nGrade: %12d\nAverage: %22.1f%%\n", lastName, firstName, gradeLevel, average);
+    }
+    
+    private void checkCollision()
+    {
+        Actor otherStudent = getOneIntersectingObject(Student.class);
+        if(otherStudent !=null)
+        {
+            if(itDelay >= 200)
+            {
+                if(it == true)
+                {
+                    it = false;
+                    
+                    setImage("ppl2.png");
+                    
+                    itDelay = 0;
+                }
+                else
+                {
+                    it = true;
+                    
+                    setImage("ppl1.png");
+                    
+                    itDelay = 0;
+                    
+                }
+            }
+                                                               
+        }
+    }
+    
+    
+    private void checkKeyPress()
+    {
+        if(Greenfoot.isKeyDown( upKey ) )
+        {
+            setLocation( getX(), getY() -3 );
+            
+        }
+        
+        if(Greenfoot.isKeyDown( downKey ) )
+        {
+            setLocation( getX(), getY() +3 );
+        }
+        
+        
+    
+        if(Greenfoot.isKeyDown( leftKey ) )
+        {
+            setLocation( getX()-3, getY() );
+            
+        }
+        
+        if(Greenfoot.isKeyDown( rightKey ) )
+        {
+            setLocation( getX()+3, getY() );
+        }
+      
+    }   
+           
+            
+    /**
+     * Act - do whatever the Student wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        checkCollision();
+        
+        checkKeyPress();
+        
+        if( Greenfoot.mouseClicked(this) )
+        {
+            System.out.println( toString() );
+        }
+        
+        itDelay++;
+    }    
+}
